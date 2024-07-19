0831b77c97cada1b041283bfa94e2d3f5ede6fbd
Ahmed Layachi <ahmed.layachi@nirusolutions.de>
2024-07-19 11:35:13 +0200

    solution common configurations

---
+++ CTBX/src/AbsenceManagement/Common/IdGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Eventuous;
+
+namespace CTBX.AbsenceManagement.Core.Common;
+
+public record Id<T> : Id
+{
+    public Id() : base($"{typeof(T).Name}-{Guid.NewGuid()}")
+    {
+        
+    }
+
+    public Id(string Value) : base(Value) { }
+}
