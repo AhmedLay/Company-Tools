c612353ee807173d52d06db40616fe6000ad7156
Youssef <youssef.bendriouch@nirusolutions.de>
2024-12-18 14:12:04 +0000

    UI working

--- src/CTBX.WebPortal/CTBX.WebPortal.Client/Registrations.cs
+++ src/CTBX.WebPortal/CTBX.WebPortal.Client/Registrations.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using CTBX.EmployeesImport.UI;
+using CTBX.ImportHolidays.UI;
+using CTBX.ImportHoliday.UI;
+
 
 public static class Registrations
 {
     public static WebAssemblyHostBuilder RegisterFeatures(this WebAssemblyHostBuilder builder)
     {
-        builder.Services.RegisterServices(builder.HostEnvironment.BaseAddress);
+        EmployeesImportRegistrations.RegisterServices(builder.Services, builder.HostEnvironment.BaseAddress);
+        ImportHolidaysRegistration.RegisterServices(builder.Services, builder.HostEnvironment.BaseAddress);
         return builder;
     }
 }
