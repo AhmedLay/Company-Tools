fe28326f5aadd3ebc945a029d17034cf4cf4c954
Hoang Duy Nguyen <hoang.nguyen003@stud.fh-dortmund.de>
2024-12-20 17:36:32 +0100

    preparing the refactoring for the services

--- src/CTBX.EmployeesImport.Backend/FileImporter.cs
+++ src/CTBX.EmployeesImport.Backend/FileImporter.cs
@@ -1,25 +1,43 @@
 ﻿
 
+using Microsoft.Extensions.Logging;
+
 namespace CTBX.EmployeesImport.Backend
 {
     public class FileImporter
     {
         private readonly FileImportService _fileImportService;
-        public FileImporter(FileImportService fileImportService)
+        private readonly ILogger<FileImportService> _logger;
+
+        public FileImporter(FileImportService fileImportService, ILogger<FileImportService> logger)
         {
             _fileImportService = fileImportService;
+            _logger = logger;
         }
 
         public async Task ImportEmployeeFromFile()
         {
-            Console.WriteLine($"Start processing file");
+            _logger.LogInformation("Start processing files");
             var pendingFiles = await _fileImportService.GetPendingFiles();
             foreach (var file in pendingFiles)
             {
                 await _fileImportService.UpdateFileStatus(file.Id, "In Progress");
-                await _fileImportService.ImportEmployeeFromFile(file.FilePath);
-                await _fileImportService.UpdateFileStatus(file.Id, "Completed");
-                await _fileImportService.DeleteFileFromFolder(file.FilePath);
+                _logger.LogDebug("Processing file {fileName}.", file.FileName);
+                try
+                {
+                    await _fileImportService.ImportEmployeeFromFile(file.FilePath);
+                    await _fileImportService.UpdateFileStatus(file.Id, "Completed");
+                    await _fileImportService.DeleteFileFromFolder(file.FilePath);
+                    _logger.LogInformation("{fileName} successfully processed.", file.FileName);
+
+                }
+                catch(Exception ex)
+                {
+                    await _fileImportService.UpdateFileStatus(file.Id, "Failed");
+                    await _fileImportService.DeleteFileFromFolder(file.FilePath);
+                    _logger.LogError(ex, "{fileName} failed.", file.FileName);
+
+                }
             }
 
         }
