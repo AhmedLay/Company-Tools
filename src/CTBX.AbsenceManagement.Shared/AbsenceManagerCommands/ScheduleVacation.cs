5ae5d3ee4694e38eefb7e8ec8b5948e155c364d2
Hoang Duy Nguyen <duy.nguyen@nirusolutions.de>
2025-02-20 14:23:45 +0100

    New version

---
+++ src/CTBX.AbsenceManagement.Shared/AbsenceManagerCommands/ScheduleVacation.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalApiArchitecture.Application.Commands;
+
+public record ScheduleVacation(
+string Id,
+int EmployeeId,
+DateTimeOffset From,
+DateTimeOffset To,
+string Comment,
+DateTimeOffset ScheduledAt);
+
